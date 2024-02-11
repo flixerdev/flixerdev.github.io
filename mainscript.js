bfb1a3a3d386d1a2af1305d726600756cc08d59e
FlixerDev <106023217+flixerdev@users.noreply.github.com>
2024-02-11 14:25:55 +0100

    fixed bugs

--- mainscript.js
+++ mainscript.js
@@ -2,7 +2,7 @@ const joinDiscord = document.querySelectorAll("#join-discord");
 joinDiscord.forEach((jd) => {
   jd.addEventListener("click", async (e) => {
     const url = await (
-      await fetch("https://int.icegenerator.xyz/api/discord-link")
+      await fetch("https://discord.gg/freegenerator")
     ).json();
     window.location = url.url;
   });
